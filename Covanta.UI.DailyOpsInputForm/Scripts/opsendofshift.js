cd12224d43431e2ceddad1d0263849c7550f1bae
CovantaDataManagement <clink@covanta.com>
2023-11-08 12:34:07 -0500

    RITM1026096 C. Link 2023-11-08

---
+++ Covanta.UI.DailyOpsInputForm/Scripts/opsendofshift.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+    function showEnvironmentalExplanation() {
+        //RITM1026096 C. Link 2023-11-08 Add button for conditional link to End of Shift Report if user selects Yes
+    }
+</script>
